825c0081fff27d842acf2fc1450c4bff7adf6273
ronald <ronaldjunior2@hotmail.com>
2021-06-07 16:06:35 -0300

    preparando review

--- src/components/general/Go/styles.js
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-
-const StyledLink = styled(Link)`
-    color: #52B6FF;
-    font-size: 14px;
-`;
-
-export default StyledLink;
--- src/components/general/Message/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const StyledTitle = styled.p`
-    color: ${props => props.color ? props.color : "#666666"};
-    font-size: ${props => props.size ? props.size : "18px"};
-`;
-
-export default StyledTitle;
